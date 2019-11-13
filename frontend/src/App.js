e71f30704160feaa634df70347efa6804e75db71
github username <vchoe89@gmail.com>
2019-11-13 11:01:16 -0500

    added category styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,18 +29,209 @@ function App() {
   const [courses, setCourses] = useState([])
   const [categories, setCategories] = useState([])
   const [currentUser, setCurrentUser] = useState({
-    id: 189,
-    name: "Garret Fahey",
-    address: "40244 Archie Street",
-    birthday: "1998-04-08",
-    email: "arie_frami@example.net",
-    phone_number: "265.239.4954",
-    profile_image: null,
-    education: "South Krajcik",
-    skills: null,
-    travel_distance: null,
-    bio: null,
-    booked_courses: [ ]
+    id: 186,
+name: "Neoma Zulauf",
+address: "21089 Stracke Heights",
+birthday: "1976-04-15",
+email: "arnoldo@example.com",
+phone_number: "1-594-545-1956",
+profile_image: null,
+education: "North Turner Institute",
+skills: null,
+travel_distance: null,
+bio: null,
+booked_courses: [
+{
+id: 210,
+name: "Private Tennis Lessons",
+hourly_rate: 30,
+location: "Reston, VA",
+created_at: "2019-11-11T14:28:05.446Z",
+updated_at: "2019-11-11T14:28:05.446Z",
+category_id: 115,
+instructor_id: 193,
+own_review: {
+id: 7,
+course_id: 210,
+content: null,
+rating: null,
+created_at: "2019-11-12T19:06:16.291Z",
+updated_at: "2019-11-12T19:06:16.291Z",
+customer_id: 186
+}
+},
+{
+id: 205,
+name: "Private Cello Lessons",
+hourly_rate: 20,
+location: "Silver Spring, MD",
+created_at: "2019-11-11T14:28:05.423Z",
+updated_at: "2019-11-11T14:28:05.423Z",
+category_id: 114,
+instructor_id: 190,
+own_review: {
+id: 8,
+course_id: 205,
+content: null,
+rating: null,
+created_at: "2019-11-12T19:11:54.713Z",
+updated_at: "2019-11-12T19:11:54.713Z",
+customer_id: 186
+}
+},
+{
+id: 203,
+name: "Basic Science",
+hourly_rate: 30,
+location: "Wheaton, MD",
+created_at: "2019-11-11T14:28:05.412Z",
+updated_at: "2019-11-11T14:28:05.412Z",
+category_id: 113,
+instructor_id: 189,
+own_review: {
+id: 9,
+course_id: 203,
+content: null,
+rating: null,
+created_at: "2019-11-12T19:13:04.812Z",
+updated_at: "2019-11-12T19:13:04.812Z",
+customer_id: 186
+}
+},
+{
+id: 205,
+name: "Private Cello Lessons",
+hourly_rate: 20,
+location: "Silver Spring, MD",
+created_at: "2019-11-11T14:28:05.423Z",
+updated_at: "2019-11-11T14:28:05.423Z",
+category_id: 114,
+instructor_id: 190,
+own_review: {
+id: 8,
+course_id: 205,
+content: null,
+rating: null,
+created_at: "2019-11-12T19:11:54.713Z",
+updated_at: "2019-11-12T19:11:54.713Z",
+customer_id: 186
+}
+},
+{
+id: 208,
+name: "Private Soccer Lessons",
+hourly_rate: 30,
+location: "Washington, DC",
+created_at: "2019-11-11T14:28:05.436Z",
+updated_at: "2019-11-11T14:28:05.436Z",
+category_id: 115,
+instructor_id: 193,
+own_review: {
+id: 16,
+course_id: 208,
+content: null,
+rating: null,
+created_at: "2019-11-12T20:24:22.215Z",
+updated_at: "2019-11-12T20:24:22.215Z",
+customer_id: 186
+}
+},
+{
+id: 200,
+name: "Advanced Writing",
+hourly_rate: 35,
+location: "Aspen Hill, MD",
+created_at: "2019-11-11T14:28:05.398Z",
+updated_at: "2019-11-11T14:28:05.398Z",
+category_id: 112,
+instructor_id: 188,
+own_review: {
+id: 17,
+course_id: 200,
+content: null,
+rating: null,
+created_at: "2019-11-12T20:25:08.731Z",
+updated_at: "2019-11-12T20:25:08.731Z",
+customer_id: 186
+}
+},
+{
+id: 206,
+name: "Private Piano Lessons",
+hourly_rate: 30,
+location: "North Bethesda, MD",
+created_at: "2019-11-11T14:28:05.427Z",
+updated_at: "2019-11-11T14:28:05.427Z",
+category_id: 114,
+instructor_id: 191,
+own_review: {
+id: 18,
+course_id: 206,
+content: null,
+rating: null,
+created_at: "2019-11-12T20:32:25.462Z",
+updated_at: "2019-11-12T20:32:25.462Z",
+customer_id: 186
+}
+},
+{
+id: 210,
+name: "Private Tennis Lessons",
+hourly_rate: 30,
+location: "Reston, VA",
+created_at: "2019-11-11T14:28:05.446Z",
+updated_at: "2019-11-11T14:28:05.446Z",
+category_id: 115,
+instructor_id: 193,
+own_review: {
+id: 7,
+course_id: 210,
+content: null,
+rating: null,
+created_at: "2019-11-12T19:06:16.291Z",
+updated_at: "2019-11-12T19:06:16.291Z",
+customer_id: 186
+}
+},
+{
+id: 205,
+name: "Private Cello Lessons",
+hourly_rate: 20,
+location: "Silver Spring, MD",
+created_at: "2019-11-11T14:28:05.423Z",
+updated_at: "2019-11-11T14:28:05.423Z",
+category_id: 114,
+instructor_id: 190,
+own_review: {
+id: 8,
+course_id: 205,
+content: null,
+rating: null,
+created_at: "2019-11-12T19:11:54.713Z",
+updated_at: "2019-11-12T19:11:54.713Z",
+customer_id: 186
+}
+},
+{
+id: 208,
+name: "Private Soccer Lessons",
+hourly_rate: 30,
+location: "Washington, DC",
+created_at: "2019-11-11T14:28:05.436Z",
+updated_at: "2019-11-11T14:28:05.436Z",
+category_id: 115,
+instructor_id: 193,
+own_review: {
+id: 16,
+course_id: 208,
+content: null,
+rating: null,
+created_at: "2019-11-12T20:24:22.215Z",
+updated_at: "2019-11-12T20:24:22.215Z",
+customer_id: 186
+}
+}
+]
 })
   const [bookedCourses, setBookedCourses] = useState([])
   const [users, setUsers] = useState([])
@@ -78,8 +269,8 @@ function App() {
     .then(r => r.json())
     .then(newCourseObj =>
       setCourses(
+        console.log(newCourseObj)
 
-        
         // [...courses, {...newCourseObj, instructor: {...currentUser}}]
       )
     )
--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Animated} from "react-animated-css";
 import {NavLink} from 'react-router-dom';
-import {Container, Image} from 'semantic-ui-react'
+import {Container, Image, Header} from 'semantic-ui-react'
 import Navbar from './Navbar.js'
 import Footer from './Footer.js'
 
@@ -13,7 +13,7 @@ function Welcome(){
         <img className="welcome image" src='https://images.pexels.com/photos/267586/pexels-photo-267586.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'  />
             <Animated animationIn="fadeInLeft" animationInDuration={2500} isVisible={true}>
                 <div>
-                  <h2 className='welcome'>Welcome to Academia</h2>
+                  <h2 className='welcome'>welcome to academia</h2>
                 </div>
             </Animated>
             <Animated animationIn='fadeInRight' animationInDuration={2500} isVisible={true}>
