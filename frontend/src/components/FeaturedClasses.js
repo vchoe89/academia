de60956bde52e8fa668b95ac66362b1098e33e33
github username <github email address>
2019-12-10 14:01:00 -0500

    updated db

--- frontend/src/components/FeaturedClasses.js
+++ frontend/src/components/FeaturedClasses.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import {Animated} from "react-animated-css";
+import CourseCard from './CourseCard.js'
 
 
-const FeaturedClasses = () => {
+const FeaturedClasses = (props) => {
 
   return(
     <div id='featured-component'>
       <h1 id='featured-header'>Featured Classes</h1>
+        {props.courses.map(course => {
+          return <CourseCard course={course}/>
+        })
+        }
         
-
-
     </div>
 
 
