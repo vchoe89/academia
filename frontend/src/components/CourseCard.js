73dfc217fea8a8a0d122a1f00b3af4c6652fb686
Victor Choe <vchoe89@gmail.com>
2021-06-16 19:45:20 -0400

    back button added

--- frontend/src/components/CourseCard.js
+++ frontend/src/components/CourseCard.js
@@ -1,21 +1,35 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import CourseDetails from './CourseDetails.js'
-import {Card} from 'semantic-ui-react'
+import {Card, Button } from 'semantic-ui-react'
 
 const CourseCard = (props) => {
 
 
   return(
-      <div id='course-page'>
+    <div id='course-page'>
+      <div id='course-page-container'>
       <Card.Group  itemsPerRow={4}>
         {
         props.courses.map(course => {
       return <CourseDetails button={props.button} currentUser={props.currentUser} bookCourse={props.bookCourse} course={course}/>
-    })
-  }
+        })
+      }
       </Card.Group>
+
       </div>
+      <Button negative>Back</Button>
+    </div>
+
   )
 }
 
 export default CourseCard;
+//
+// <button id='back-button' basic color='red' size='medium' onClick={
+//     ()=>{
+//       console.log('click');
+//     }
+//   }>
+//   Go Back
+//   </button>
